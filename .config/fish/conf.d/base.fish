6949418fb4eb85500cb2d2bb8c444bb380a46699
Jacob Hayes <jrh@jacobhayes.dev>
2025-03-26 21:56:41 -0500

    Update fish colors

--- .config/fish/conf.d/base.fish
+++ .config/fish/conf.d/base.fish
@@ -3,3 +3,7 @@ alias ji="cdi" # cd is overwritten with zoxide
 
 set fish_greeting ""
 set fish_key_bindings fish_vi_key_bindings
+
+set -U fish_color_command blue
+set -U fish_color_keyword blue
+set -U fish_color_option purple
